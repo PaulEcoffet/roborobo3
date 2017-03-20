7e5d018f63c244fdd2ba2eec9702930c8c8aed8d
Théotime Grohens <theotime.grohens@ens.fr>
2017-03-20 09:49:24 +0100

    First step for supporting object-object collisions
    Former-commit-id: 3d5cfb3a0dbc3ae9805e68a862483bef6e21d10e

--- roborobo3/src/ext/MovingObject.cpp
+++ roborobo3/src/ext/MovingObject.cpp
@@ -25,31 +25,33 @@ bool MovingObject::canRegister( int x, int y )
 
 void MovingObject::step()
 {
-    if (_robotImpulses.size() > 0)
+    if (_impulses.size() > 0)
     {
  //       printf("[DEBUG] Moving object %d\n", _id);
         double impXtot = 0, impYtot = 0;
         
         // impulses are in polar form, and angles in degrees
-        for (auto& imp : _robotImpulses) {
+        for (auto& imp : _impulses) {
             //We only want the normal component of the speed wrt the centers of mass
             // v: robot speed vector, u: robot-object vector
             
             double vx = imp.second.x*cos(imp.second.y * M_PI / 180.0);
             double vy = imp.second.x*sin(imp.second.y * M_PI / 180.0);
             
-            Robot *robot = gWorld->getRobot(imp.first);
-            double ux = (double)_xCenterPixel - robot->getWorldModel()->getXReal();
-            double uy = (double)_yCenterPixel - robot->getWorldModel()->getYReal();
-            double sqnorm = ux*ux + uy*uy;
-            
-            double impX = (vx*ux+vy*uy)*ux/sqnorm;
-            double impY = (vx*ux+vy*uy)*uy/sqnorm;
-            
-            impXtot += impX;
-            impYtot += impY;
+            if (imp.first >= gRobotIndexStartOffset) {
+                Robot *robot = gWorld->getRobot(imp.first-gRobotIndexStartOffset);
+                double ux = (double)_xCenterPixel - robot->getWorldModel()->getXReal();
+                double uy = (double)_yCenterPixel - robot->getWorldModel()->getYReal();
+                double sqnorm = ux*ux + uy*uy;
+                
+                double impX = (vx*ux+vy*uy)*ux/sqnorm;
+                double impY = (vx*ux+vy*uy)*uy/sqnorm;
+                
+                impXtot += impX;
+                impYtot += impY;
+            }
         }
-        _robotImpulses.clear();
+        _impulses.clear();
         
         int dx = 0, dy = 0;
         
@@ -64,8 +66,8 @@ void MovingObject::step()
 
         int newX = _xCenterPixel + dx;
         int newY = _yCenterPixel + dy;
-        printf("[DEBUG] Impulses: %lf->%d (x) %lf->%d (y).\n", impXtot, dx,
-               impYtot, dy);
+ //       printf("[DEBUG] Impulses: %lf->%d (x) %lf->%d (y).\n", impXtot, dx,
+ //              impYtot, dy);
         
         unregisterObject();
         hide();
@@ -88,9 +90,10 @@ void MovingObject::show() {
 
 void MovingObject::isPushed( int __idAgent, Point2d __speed)
 {
-//    printf("[DEBUG]: object %d is being pushed by agent %d.\n", _id, __idAgent);
-    if (_robotImpulses.count(__idAgent) == 0)
-        _robotImpulses.insert(std::pair<int, Point2d>(__idAgent, __speed));
+
+    printf("[DEBUG]: object %d is being pushed by agent %d.\n", _id, __idAgent);
+    if (_impulses.count(__idAgent) == 0)
+        _impulses.insert(std::pair<int, Point2d>(__idAgent, __speed));
 }
 
 void MovingObject::isTouched( int __idAgent )
