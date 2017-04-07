3a983b04071279d0bcef451d0abcab731d0ee92a
Théotime Grohens <theotime.grohens@ens.fr>
2017-04-07 16:40:45 +0200

    Revert "Removed MovingObject files."
    This reverts commit d0aa94b58addbc2f4584fe36cbfdcac99935ab6a.

---
+++ roborobo3/src/ext/MovingObject.cpp
@@ -0,0 +1,37 @@
+#include "World/MovingObject.h"
+
+#include "RoboroboMain/roborobo.h"
+#include "Utilities/Misc.h"
+#include "World/World.h"
+
+#include <iomanip>
+
+MovingObject::MovingObject( int __id ) : CircleObject ( __id )
+{
+    setType(5);
+}
+
+void MovingObject::step()
+{
+    CircleObject::step();
+}
+
+void MovingObject::show() {
+    //	printf("Displaying moving object #%d\n", _id);
+    CircleObject::show();
+}
+
+void MovingObject::isPushed( int __idAgent, Point2d __speed)
+{
+    CircleObject::isPushed(__idAgent, __speed);
+}
+
+void MovingObject::isTouched( int __idAgent )
+{
+    
+}
+
+void MovingObject::isWalked( int __idAgent )
+{
+    
+}
