2abf54364d4acc0e89d8d5e00a0af112ad07bebf
Théotime Grohens <theotime.grohens@ens.fr>
2017-04-07 17:39:06 +0200

    Robots now get fitness if they're closed to an object that moved.
    Former-commit-id: d12cd1297685af8cc6615415661fbdbe8f43ef68

--- roborobo3/prj/MovingNS/src/MovingNSAgentObserver.cpp
+++ roborobo3/prj/MovingNS/src/MovingNSAgentObserver.cpp
@@ -35,9 +35,20 @@ void MovingNSAgentObserver::step()
     if ( _wm->isAlive() && _wm->getDidPush() )
     {
         MovingNSController *ctl = dynamic_cast<MovingNSController *>(gWorld->getRobot(_wm->getId())->getController());
-        ctl->increaseFitness(1);
+//        ctl->increaseFitness(1);
         ctl->updatePushes();
     }
+    
+    // See whether we're close to an object
+    
+    int targetIndex = _wm->getGroundSensorValue();
+    if ( targetIndex >= gPhysicalObjectIndexStartOffset && targetIndex < gPhysicalObjectIndexStartOffset + (int)gPhysicalObjects.size() )   // ground sensor is upon a physical object (OR: on a place marked with this physical object footprint, cf. groundsensorvalues image)
+    {
+        targetIndex = targetIndex - gPhysicalObjectIndexStartOffset;
+        //std::cout << "[DEBUG] #" << _wm->getId() << " walked upon " << targetIndex << "\n";
+        gPhysicalObjects[targetIndex]->isWalked(_wm->getId());
+    }
+    
     _wm->setDidPush(false);
     _wm->setTriedPushing(false);
 }
