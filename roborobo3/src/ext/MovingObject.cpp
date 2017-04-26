24a8b526e42dfa127a026291eea6bf22f8c07897
Théotime Grohens <theotime.grohens@ens.fr>
2017-04-26 16:18:14 +0200

    Match the changes related to gStuckMovableObjects
    Former-commit-id: 2e020628638bec3abe1b554e70a4e1b250188a43

--- roborobo3/src/ext/MovingObject.cpp
+++ roborobo3/src/ext/MovingObject.cpp
@@ -21,14 +21,21 @@ void MovingObject::step()
 
     CircleObject::step(); //handles movement, and sets _didMove
 
-    double movement = sqrt((oldX-_xReal)*(oldX-_xReal) + (oldY-_yReal)*(oldY-_yReal));
+    double movement = sqrt((oldX-_desiredX)*(oldX-_desiredX) + (oldY-_desiredY)*(oldY-_desiredY));
 	int nbRobots = static_cast<int>(_nearbyRobots.size());
 	double coeff = 1.0/(1.0+pow(nbRobots-2, 2)); // \frac{1}{1+(nbRobots-2)^2}
 	double gain = movement * coeff;
+    
+    if (gStuckMovableObjects) // get back into place!
+    {
+        _xReal = oldX;
+        _yReal = oldY;
+    }
 
     for (auto robotID: _nearbyRobots)
     {
         Robot *robot = gWorld->getRobot(robotID);
+        // See ConfigurationLoader.cpp for a way to dynamically adjust to which project we're running
         MonoRobotController *ctl = dynamic_cast<MonoRobotController *>(robot->getController());
         ctl->wasNearObject(_didMove, gain, nbRobots);
     }
@@ -42,6 +49,7 @@ void MovingObject::show() {
 
 void MovingObject::isPushed( int __idAgent, Point2d __speed)
 {
+    printf("[DEBUG] Object %d is being pushed by agent %d\n", _id, __idAgent);
     CircleObject::isPushed(__idAgent, __speed);
 }
 
