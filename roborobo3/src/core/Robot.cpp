45f0d7cf0c5ac51c01ed4abfba52f0b151e5dc1a
Théotime Grohens <theotime.grohens@ens.fr>
2017-05-12 14:45:12 +0200

    clean up tuples

--- roborobo3/src/core/Robot.cpp
+++ roborobo3/src/core/Robot.cpp
@@ -684,7 +684,7 @@ bool Robot::isCollision()
                             if ( targetIndex >= gPhysicalObjectIndexStartOffset && targetIndex < gRobotIndexStartOffset)   // physical object
                             {
                                 targetIndex = targetIndex - gPhysicalObjectIndexStartOffset;
-                                gPhysicalObjects[targetIndex]->isPushed(_wm->getId()+gRobotIndexStartOffset, std::tuple<double, double>(_wm->_agentAbsoluteLinearSpeed, _wm->_agentAbsoluteOrientation));
+                                gPhysicalObjects[targetIndex]->isPushed(_wm->getId()+gRobotIndexStartOffset, std::tie(_wm->_agentAbsoluteLinearSpeed, _wm->_agentAbsoluteOrientation));
                             }
                         }
 						return true;
