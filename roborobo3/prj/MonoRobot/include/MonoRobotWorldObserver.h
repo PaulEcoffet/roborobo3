8e3ddf2e19b285680409cb2737a2b7cf617c7059
Théotime Grohens <theotime.grohens@ens.fr>
2017-06-13 10:45:35 +0200

    Partner choice experiment, first version

--- roborobo3/prj/MonoRobot/include/MonoRobotWorldObserver.h
+++ roborobo3/prj/MonoRobot/include/MonoRobotWorldObserver.h
@@ -27,10 +27,6 @@ protected:
     int _evaluationCount;
     int _evaluationItCount;
     
-    std::pair<int, double> _objectProperties[4];
-    std::vector<int> _objectOrder;
-    Uint8 _landmarkColors[4][3] = { {0x00, 0x99, 0xFF}, {0xFF, 0x80, 0x00}, {0xFF, 0x69, 0xB4}, {0xFF, 0x00, 0x00} };
-
     std::ofstream _logFile;
     LogManager *_logManager; // Our own little logfile
     
@@ -47,10 +43,6 @@ public:
     
     virtual int getGenerationItCount() { return _evaluationItCount; }
     
-    std::vector<int> getObjectOrder() { return _objectOrder; }
-    
-    std::pair<int, double> getObjectProperties(int __obj) { return _objectProperties[__obj]; }
-
 };
 
 #endif
