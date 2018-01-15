e3d5dc73b3096aa76d1123a0bcbab99c0ca90a54
Paul Ecoffet <ecoffet.paul@gmail.com>
2018-01-15 14:01:13 +0100

    new run script
    Former-commit-id: 2d5d953ca0718c1e384800d6fb87209ba01bb6d8

--- roborobo3/runcoopfixed2.sh
+++ roborobo3/runcoopfixed2.sh
@@ -1,5 +1,43 @@
+#clean
+rm -rfI ../logs/coopfixed2_commonpot/
+# CoopFixed2
+
+mkdir -p ../logs/coopfixed2_commonpot//fake_rep_0/screenshots
+nohup python pyevoroborobo.py -e cmaes -l config/CoopOpportunity2FixedFakeBots.properties -o ../logs/coopfixed2_commonpot//fake_rep_0/ -b > ../logs/coopfixed2_commonpot//fake_rep_0/stdout.txt 2> ../logs/coopfixed2_commonpot//fake_rep_0/stderr.txt &
+sleep 1.5
+mkdir -p ../logs/coopfixed2_commonpot//nofake_rep_0/screenshots
+nohup python pyevoroborobo.py -e cmaes -l config/CoopOpportunity2Fixed.properties -o ../logs/coopfixed2_commonpot//nofake_rep_0/ -b > ../logs/coopfixed2_commonpot//nofake_rep_0/stdout.txt 2> ../logs/coopfixed2_commonpot//nofake_rep_0/stderr.txt &
+sleep 1.5
+mkdir -p ../logs/coopfixed2_commonpot//fake_rep_1/screenshots
+nohup python pyevoroborobo.py -e cmaes -l config/CoopOpportunity2FixedFakeBots.properties -o ../logs/coopfixed2_commonpot//fake_rep_1/ -b > ../logs/coopfixed2_commonpot//fake_rep_1/stdout.txt 2> ../logs/coopfixed2_commonpot//fake_rep_1/stderr.txt &
+sleep 1.5
+mkdir -p ../logs/coopfixed2_commonpot//nofake_rep_1/screenshots
+nohup python pyevoroborobo.py -e cmaes -l config/CoopOpportunity2Fixed.properties -o ../logs/coopfixed2_commonpot//nofake_rep_1/ -b > ../logs/coopfixed2_commonpot//nofake_rep_1/stdout.txt 2> ../logs/coopfixed2_commonpot//nofake_rep_1/stderr.txt &
+sleep 1.5
+mkdir -p ../logs/coopfixed2_commonpot//fake_rep_2/screenshots
+nohup python pyevoroborobo.py -e cmaes -l config/CoopOpportunity2FixedFakeBots.properties -o ../logs/coopfixed2_commonpot//fake_rep_2/ -b > ../logs/coopfixed2_commonpot//fake_rep_2/stdout.txt 2> ../logs/coopfixed2_commonpot//fake_rep_2/stderr.txt &
+sleep 1.5
+mkdir -p ../logs/coopfixed2_commonpot//nofake_rep_2/screenshots
+nohup python pyevoroborobo.py -e cmaes -l config/CoopOpportunity2Fixed.properties -o ../logs/coopfixed2_commonpot//nofake_rep_2/ -b > ../logs/coopfixed2_commonpot//nofake_rep_2/stdout.txt 2> ../logs/coopfixed2_commonpot//nofake_rep_2/stderr.txt &
+sleep 1.5
+wait
+
 # wait until all simulations are over before analysis
 wait
 
 # Analysis
+
+nohup ./roborobo -l config/CoopOpportunity2FixedAnalysis.properties -o ../logs/coopfixed2_commonpot//fake_rep_0/ -b > ../logs/coopfixed2_commonpot//fake_rep_0/stdout_analysis.txt 2> ../logs/coopfixed2_commonpot//fake_rep_0/stderr_analysis.txt &
+sleep 1.5
+nohup ./roborobo -l config/CoopOpportunity2FixedAnalysis.properties -o ../logs/coopfixed2_commonpot//nofake_rep_0/ -b > ../logs/coopfixed2_commonpot//nofake_rep_0/stdout_analysis.txt 2> ../logs/coopfixed2_commonpot//nofake_rep_0/stderr_analysis.txt &
+sleep 1.5
+nohup ./roborobo -l config/CoopOpportunity2FixedAnalysis.properties -o ../logs/coopfixed2_commonpot//fake_rep_1/ -b > ../logs/coopfixed2_commonpot//fake_rep_1/stdout_analysis.txt 2> ../logs/coopfixed2_commonpot//fake_rep_1/stderr_analysis.txt &
+sleep 1.5
+nohup ./roborobo -l config/CoopOpportunity2FixedAnalysis.properties -o ../logs/coopfixed2_commonpot//nofake_rep_1/ -b > ../logs/coopfixed2_commonpot//nofake_rep_1/stdout_analysis.txt 2> ../logs/coopfixed2_commonpot//nofake_rep_1/stderr_analysis.txt &
+sleep 1.5
+nohup ./roborobo -l config/CoopOpportunity2FixedAnalysis.properties -o ../logs/coopfixed2_commonpot//fake_rep_2/ -b > ../logs/coopfixed2_commonpot//fake_rep_2/stdout_analysis.txt 2> ../logs/coopfixed2_commonpot//fake_rep_2/stderr_analysis.txt &
+sleep 1.5
+nohup ./roborobo -l config/CoopOpportunity2FixedAnalysis.properties -o ../logs/coopfixed2_commonpot//nofake_rep_2/ -b > ../logs/coopfixed2_commonpot//nofake_rep_2/stdout_analysis.txt 2> ../logs/coopfixed2_commonpot//nofake_rep_2/stderr_analysis.txt &
+sleep 1.5
+wait
 
