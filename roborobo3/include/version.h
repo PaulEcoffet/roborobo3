9f9b6df8fd2c462b83a21d90da91264e6829fb0e
Paul Ecoffet <ecoffet.paul@gmail.com>
2018-04-17 15:49:53 +0200

    new gitsha1 status without define but automatic file to speed up compilation
    Former-commit-id: 70f2d9b4031f90a661062691529b24528c7f0a92

---
+++ roborobo3/include/version.h
@@ -0,0 +1,19 @@
+//
+// Created by pecoffet on 17/04/18.
+//
+
+#ifndef ROBOROBO3_VERSION_H
+#define ROBOROBO3_VERSION_H
+
+
+#include <string>
+
+struct Version
+{
+    static const std::string GIT_SHA1;
+    static const std::string GIT_DATE;
+    static const std::string GIT_COMMIT_SUBJECT;
+};
+
+
+#endif //ROBOROBO3_VERSION_H
---
+++ roborobo3/version.cpp
@@ -0,0 +1,6 @@
+#include "version.h"
+
+
+const std::string Version::GIT_SHA1 = "eb11c7193c674a25c9b33521a4d4313365d34c97-dirty";
+const std::string Version::GIT_DATE = "Fri Apr 13 16:15:04 2018";
+const std::string Version::GIT_COMMIT_SUBJECT = "memory size can now be changed";
