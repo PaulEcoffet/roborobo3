4e2341c696262b18047cf65eb8d7be972639f629
Paul Ecoffet <ecoffet.paul@gmail.com>
2018-09-28 14:29:40 +0200

    Normalize inputs
    Former-commit-id: b444d9cb982addec2f0af2bb75d1fb5e4ce763ad

--- roborobo3/prj/CoopFixed2/src/CoopFixed2Controller.cpp
+++ roborobo3/prj/CoopFixed2/src/CoopFixed2Controller.cpp
@@ -283,12 +283,12 @@ std::vector<double> CoopFixed2Controller::getGameInputs() const {
     }
     if (CoopFixed2SharedData::totalInvAsInput)
     {
-        inputs[i++] = m_wm->meanLastTotalInvest();
+        inputs[i++] = m_wm->meanLastTotalInvest() / CoopFixed2SharedData::maxCoop;
         if (fill_names) inputnames.emplace_back("mean total inv");
 
     }
     if (CoopFixed2SharedData::ownInvAsInput) {
-        inputs[i++] = m_wm->meanLastOwnInvest();
+        inputs[i++] = m_wm->meanLastOwnInvest() / CoopFixed2SharedData::maxCoop;
         if (fill_names) inputnames.emplace_back("mean own inv");
     }
 
@@ -296,7 +296,7 @@ std::vector<double> CoopFixed2Controller::getGameInputs() const {
      * introspection inputs
      */
     if (CoopFixed2SharedData::selfAAsInput) {
-        inputs[i++] = m_wm->selfA;
+        inputs[i++] = (m_wm->selfA  - CoopFixed2SharedData::meanA) / CoopFixed2SharedData::stdA;
         if (fill_names) inputnames.emplace_back("own A");
 
     }
