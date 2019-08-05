915d17d05122ab5676e2ee8a6753007d691d2e86
Paul Ecoffet <ecoffet.paul@gmail.com>
2019-08-05 17:31:30 +0200

    New analysis system for negociate
    Former-commit-id: da313f70fcd306c5cb1e986dd5d1e7185a605bb4

--- roborobo3/prj/Negociate/include/NegociateWorldObserver.h
+++ roborobo3/prj/Negociate/include/NegociateWorldObserver.h
@@ -6,12 +6,16 @@
 #define ROBOROBO3_NEGOCIATEWORLDOBSERVER_H
 
 
+
 #include <Observers/WorldObserver.h>
 #include <World/World.h>
 #include <network/PyevoInterface.h>
 #include "Utilities/LogManager.h"
 #include "json/json.hpp"
 #include "NegociateController.h"
+#include <opencv2/core.hpp>  // Basic OpenCV structures (cv::Mat)
+#include <opencv2/videoio.hpp>  // VideoWriter
+#include <opencv2/imgproc/imgproc.hpp>  // channel manipulation
 
 using json = nlohmann::json;
 
@@ -47,6 +51,8 @@ public:
 protected:
     World *m_world;
     LogManager *m_fitnessLogManager;
+
+    cv::VideoWriter outvid;
 
     int m_curEvaluationInGeneration;
     int m_curEvaluationIteration;
