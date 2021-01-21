fix wander evolution to be less sensitive to close to 0 rotation with linear last layer

roborobo3/py_example/wander_evolution.py
@@ -1,12 +1,15 @@
-from pyroborobo import Pyroborobo,  PyWorldModel, Controller, AgentObserver
+from pyroborobo import Pyroborobo, PyWorldModel, Controller, AgentObserver
 import numpy as np
 from scipy.stats import rankdata
+
+from objects import SwitchObject, GateObject
 
 
 def evaluate_network(input_, network):
     out = np.concatenate([[1], input_])
-    for elem in network:
+    for elem in network[:-1]:
         out = np.tanh(out @ elem)
+    out = out @ network[-1]  # linear output for last layer
     return out
 
 
@@ -17,22 +20,26 @@ class EvolController(Controller):
     def __init__(self, wm):
         Controller.__init__(self, wm)
         self.nb_hiddens = 10
-        self.weights = [np.random.normal(0, 1, (self.nb_sensors + 1, self.nb_hiddens)), np.random.normal(0, 1, (self.nb_hiddens, 2))]
+        self.weights = [np.random.normal(0, 1, (self.nb_sensors + 1, self.nb_hiddens)),
+                        np.random.normal(0, 1, (self.nb_hiddens, 2))]
+        self.tot_weights = np.sum([np.prod(layer.shape) for layer in self.weights])
 
     def reset(self):
         pass
 
     def step(self):
         input = self.get_all_distances()
-        out = evaluate_network(input, self.weights)
+        out = np.clip(evaluate_network(input, self.weights), -1, 1)
         self.set_translation(out[0])
         self.set_rotation(out[1])
 
     def get_flat_weights(self):
-        all_elem = []
-        for elem in self.weights:
-            all_elem.append(elem.reshape(-1))
-        return np.concatenate(all_elem)
+        all_layers = []
+        for layer in self.weights:
+            all_layers.append(layer.reshape(-1))
+        flat_layers = np.concatenate(all_layers)
+        assert (flat_layers.shape == (self.tot_weights,))
+        return flat_layers
 
     def set_weights(self, weights):
         j = 0
@@ -41,7 +48,9 @@ class EvolController(Controller):
             size = elem.size
             self.weights[i] = np.array(weights[j:(j + size)]).reshape(shape)
             j += size
-
+        # assert that we have consume all the weights needed
+        assert (j == self.tot_weights)
+        assert (j == len(weights))
 
 
 class EvolObserver(AgentObserver):
@@ -58,10 +67,11 @@ class EvolObserver(AgentObserver):
         speed = self.controller.translation
         rotspeed = np.abs(self.controller.rotation)
         dists = np.asarray(self.controller.get_all_distances())
+        fitdelta = 5 * speed + np.min(dists) + 4 * np.exp(-10 * rotspeed)
         if np.random.rand() < 0.0001:
             print(speed, rotspeed, dists)
-            print(speed - np.max(1 - dists) - np.abs(rotspeed))
-        self.fitness += speed - np.max(1 - dists) - 10*rotspeed
+            print(fitdelta)
+        self.fitness += fitdelta
 
 
 def get_weights(rob: Pyroborobo):
@@ -80,12 +90,13 @@ def get_fitnesses(rob: Pyroborobo):
 
 def fitprop(weights, fitnesses):
     adjust_fit = rankdata(fitnesses)
+    # adjust_fit = np.clip(fitnesses, 0.00001, None)
     normfit = adjust_fit / np.sum(adjust_fit)
     # select
     new_weights_i = np.random.choice(len(weights), len(weights), replace=True, p=normfit)
     new_weights = np.asarray(weights)[new_weights_i]
     # mutate
-    new_weights_mutate = np.random.normal(new_weights, 0.1)
+    new_weights_mutate = np.random.normal(new_weights, 0.01)
     return new_weights_mutate
 
 
@@ -103,10 +114,11 @@ def main():
     nbgen = 10000
     nbiterpergen = 400
     rob: Pyroborobo = Pyroborobo.create(
-        "config/pywander.properties",
+        "config/pywander_pyobj.properties",
         controller_class=EvolController,
         world_model_class=PyWorldModel,
         agent_observer_class=EvolObserver,
+        object_class_dict={'gate': GateObject, 'switch': SwitchObject}
     )
 
     rob.start()